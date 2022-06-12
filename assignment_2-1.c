42d0a9c18648ee52fdb98f2421edfff97f546ccd
Arijit <subhasghosh196@outlook.com>
2022-06-12 16:49:47 +0530

    adding

---
+++ assignment_2-1.c
@@ -0,0 +1,76 @@
+/* Name : Arijit Ghosh
+Dept. - CSE
+Roll : 35000121017
+To do operations with Complex numbers
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+struct complex
+{
+	float real;
+	float img;
+} c1, c2;
+
+int main()
+{
+	float mod1, argu1, mod2, argu2;
+
+	printf("** Enter Complex Number -1 **\n");
+	printf("-- Real Part: ");
+	scanf("%f", &c1.real);
+	printf("-- Imaginary Part: ");
+	scanf("%f", &c1.img);
+	printf("** Enter Complex Number -2 **\n");
+	printf("-- Real Part: ");
+	scanf("%f", &c2.real);
+	printf("-- Imaginary Part: ");
+	scanf("%f", &c2.img);
+
+	printf("Complex Number -1 : C1 - (%f) + (%f)i\n", c1.real, c1.img);
+	printf("Complex Number -2 : C2 - (%f) + (%f)i\n", c2.real, c2.img);
+
+	mod1 = pow((pow(c1.real, 2) + pow(c1.img, 2)), 0.5);
+	mod2 = pow((pow(c2.real, 2) + pow(c2.img, 2)), 0.5);
+
+	argu1 = atan(c1.img / c1.real);
+	argu2 = atan(c2.img / c2.real);
+
+	printf("Modulus of Complex Number-1 : |C1| - %f\n", mod1);
+	printf("Modulus of Complex Number-2 : |C2| - %f\n", mod2);
+
+	printf("Conjugate of Complex Number-1: C1 = (%f) + (%f)i\n", c1.real, -1 * c1.img);
+	printf("Conjugate of Complex Number-2: C2 = (%f) + (%f)i\n", c2.real, -1 * c2.img);
+
+	printf("Argument of Complex Number-1: ARG(C1)= %f\n", argu1);
+	printf("Argument of Complex Number-2: ARG(C1) = %f\n", argu2);
+
+	printf("Complex Number after Addition: (C1+C2) = (%f) + (%f)i\n", c1.real + c2.real, c1.img + c2.img);
+	printf("Complex Number after Subtraction: (C1-C2) = (%f) + (%f)i\n", c1.real - c2.real, c1.img - c2.img);
+	printf("Complex Number after Multiplication: (C1*C2) = (%f) + (%f)i\n", (c1.real * c2.real - c1.img * c2.img), (c1.real * c2.img + c2.real * c1.img));
+	printf("Complex Number after Divison: (C1/C2) = (%f) + (%f)i\n", (c1.real * c2.real + c1.img * c2.img) / (pow(c2.real, 2) + pow(c2.img, 2)), (c1.img * c2.real - c1.real * c2.img) / (pow(c2.real, 2) + pow(c2.img, 2)));
+	printf("Square of Complex Number-1 : C1^2 - (%f) + (%f)i\n", pow(c1.real, 2) - pow(c1.img, 2), 2 * c1.real * c1.img);
+	printf("Square of Complex Number-2 : C2^2 - (%f) + (%f)i\n", pow(c2.real, 2) - pow(c2.img, 2), 2 * c2.real * c2.img);
+
+	printf("Cube of Complex Number-1 : C1^3 - (%f) + (%f)i\n", (pow(c1.real, 3) - 3 * c1.real * pow(c1.img, 2)), 3 * pow(c1.real, 2) * c1.img - pow(c1.img, 3));
+	printf("Cube of Complex Number-2 : C2^3 - (%f) + (%f)i\n", (pow(c2.real, 3) - 3 * c2.real * pow(c2.img, 2)), 3 * pow(c2.real, 2) * c2.img - pow(c2.img, 3));
+
+	if (c1.img < 0)
+		printf("Square root of Complex Number-1 : SQRT(C1) = (+/-) [(%f) + -(%f)i]\n", sqrt((c1.real + sqrt(pow(c1.real, 2) + pow(c1.img, 2))) / 2), sqrt((-c1.real + sqrt(pow(c1.real, 2) + pow(c1.img, 2))) / 2));
+	else
+		printf("Square root of Complex Number-1 : SQRT(C1) = (+/-) [(%f) + (%f)i]\n", sqrt((c1.real + sqrt(pow(c1.real, 2) + pow(c1.img, 2))) / 2), sqrt((-c1.real + sqrt(pow(c1.real, 2) + pow(c1.img, 2))) / 2));
+
+	if (c2.img < 0)
+		printf("Square root of Complex Number-2 : SQRT(C1) = (+/-) [(%f) + -(%f)i]\n", sqrt((c2.real + sqrt(pow(c2.real, 2) + pow(c2.img, 2))) / 2), sqrt((-c2.real + sqrt(pow(c2.real, 2) + pow(c2.img, 2))) / 2));
+	else
+		printf("Square root of Complex Number-2 : SQRT(C1) = (+/-) [(%f) + (%f)i]\n", sqrt((c2.real + sqrt(pow(c2.real, 2) + pow(c2.img, 2))) / 2), sqrt((-c2.real + sqrt(pow(c2.real, 2) + pow(c2.img, 2))) / 2));
+
+	printf("Reciprocal of Complex Number-1 : 1/C1 = (%f) + (%f)i\n", c1.real / (pow(c1.real, 2) + pow(c1.img, 2)), -c1.img / (pow(c1.real, 2) + pow(c1.img, 2)));
+	printf("Reciprocal of Complex Number-2 : 1/C2 = (%f) + (%f)i\n", c2.real / (pow(c2.real, 2) + pow(c2.img, 2)), -c2.img / (pow(c2.real, 2) + pow(c2.img, 2)));
+
+	printf("Exponential of Complex Number-1 : e^-C1 = (%f) + (%f)i\n", exp(c1.real) * cos(c1.img), exp(c1.real) * sin(c1.img));
+	printf("Exponential of Complex Number-1 : e^-C2 = (%f) + (%f)i\n", exp(c2.real) * cos(c2.img), exp(c2.real) * sin(c2.img));
+
+	return 0;
+}
---
+++ assignment_2-2.c
@@ -0,0 +1,225 @@
+#include <stdio.h>
+#include <math.h>
+
+void numWord(int a)
+{
+    int rev = 0, digit, i = 0;
+    int len = (int)(log10(a) + 1);
+    int arr[len];
+    while (a != 0)
+    {
+        arr[i] = a % 10;
+        a = a / 10;
+        i++;
+    }
+    while (i != 0)
+    {
+        switch (arr[i - 1])
+        {
+        case 0:
+            printf("zero ");
+            break;
+        case 1:
+            printf("one ");
+            break;
+        case 2:
+            printf("two ");
+            break;
+        case 3:
+            printf("three ");
+            break;
+        case 4:
+            printf("four ");
+            break;
+        case 5:
+            printf("five ");
+            break;
+        case 6:
+            printf("six ");
+            break;
+        case 7:
+            printf("seven ");
+            break;
+        case 8:
+            printf("eight ");
+            break;
+        case 9:
+            printf("nine ");
+            break;
+        }
+        i--;
+    }
+    printf("\n");
+}
+
+char grade(int a)
+{
+    if (a >= 90)
+        return 'E';
+    else if (a >= 80)
+        return 'A';
+    else if (a >= 70)
+        return 'B';
+    else if (a >= 60)
+        return 'C';
+    else if (a >= 50)
+        return 'D';
+    else if (a >= 40)
+        return 'P';
+    else
+        return 'F';
+}
+char gradeBacklog(int a)
+{
+    if (a >= 90)
+        return 'A';
+    else if (a >= 80)
+        return 'B';
+    else if (a >= 70)
+        return 'C';
+    else if (a >= 60)
+        return 'D';
+    else if (a >= 50)
+        return 'P';
+    else if (a >= 40)
+        return 'P';
+    else
+        return 'F';
+}
+
+char gradeRelative(int a, int b[])
+{
+    int i = 0;
+    char gradeArr[6] = {'E', 'A', 'B', 'C', 'D', 'P'};
+    for (i = 0; i < 6; i++)
+    {
+        if (a >= b[i])
+        {
+            return gradeArr[i];
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int roll, marks, backlog, relative, i, j, error = 0, check = 0, temp1, temp2;
+    float atten;
+    int cutoff[6];
+    char gradeArr[6] = {'E', 'A', 'B', 'C', 'D', 'P'}, grd;
+
+    printf("** Enter Student Roll Number : ");
+    scanf("%d", &roll);
+
+    printf("** Enter the Attendance Percentage : ");
+    scanf("%f", &atten);
+
+    if (atten >= 80)
+    {
+        printf("** Enter Subject Marks (out of 100) : ");
+        scanf("%d", &marks);
+        if (marks <= 100)
+        {
+            printf("** Is this marks for Supplementary/Backlog Examination? (Type '0' if NOT): ");
+            scanf("%d", &backlog);
+
+            if (backlog != 0)
+            {
+                printf("** Do you want Relative Grading? (Type '0' if NOT): ");
+                scanf("%d", &relative);
+
+                if (relative != 0)
+                {
+                    for (i = 0; i < 6; i++)
+                    {
+                        printf("** Cutoff for %c = ", gradeArr[i]);
+                        scanf("%d", &cutoff[i]);
+                    }
+                    for (i = 0; i < 6; i++)
+                    {
+                        for (j = 0; j < 6; j++)
+                        {
+                            if (i != j && cutoff[i] == cutoff[j])
+                            {
+                                error = 1;
+                                temp1 = i;
+                                temp2 = j;
+                                break;
+                            }
+                        }
+                    }
+                    if (error == 1)
+                        printf("-- Error : Cutoff for '%c' is not less than Cutoff for '%c'!", gradeArr[temp1], gradeArr[temp2]);
+
+                    else
+                    {
+                        grd = gradeRelative(marks, cutoff);
+                        printf("-- The Subject Marks of %d is : ", roll);
+                        numWord(marks);
+                        printf("-- The Relative Grade of %d is : %c\n", roll, grd);
+                    }
+                }
+                else
+                {
+                    printf("-- The Subject Marks of %d is : ", roll);
+                    numWord(marks);
+                    printf("-- The Absolute Grade of %d is : %c\n", roll, gradeBacklog(marks));
+                }
+            }
+            else
+            {
+                printf("** Do you want Relative Grading? (Type '0' if NOT): ");
+                scanf("%d", &relative);
+
+                if (relative != 0)
+                {
+                    for (i = 0; i < 6; i++)
+                    {
+                        printf("** Cutoff for %c = ", gradeArr[i]);
+                        scanf("%d", &cutoff[i]);
+                    }
+                    for (i = 0; i < 6; i++)
+                    {
+                        for (j = 0; j < 6; j++)
+                        {
+                            if (i != j && cutoff[i] == cutoff[j])
+                            {
+                                error = 1;
+                                temp1 = i;
+                                temp2 = j;
+                                break;
+                            }
+                        }
+                    }
+                    if (error == 1)
+                        printf("-- Error : Cutoff for '%c' is not less than Cutoff for '%c'!", gradeArr[temp1], gradeArr[temp2]);
+
+                    else
+                    {
+                        grd = gradeRelative(marks, cutoff);
+                        printf("-- The Subject Marks of %d is : ", roll);
+                        numWord(marks);
+                        printf("-- The Relative Grade of %d is : %c\n", roll, grd);
+                    }
+                }
+                else
+                {
+                    printf("-- The Subject Marks of %d is : ", roll);
+                    numWord(marks);
+                    printf("-- The Absolute Grade of %d is : %c\n", roll, grade(marks));
+                }
+            }
+        }
+
+        else
+            printf("-- Error: Mistake in Marks Record!");
+    }
+
+    else if (atten > 100)
+        printf("-- Error : Mistake in Attendance Record!");
+
+    else
+        printf("-- The Grade of %d is : F (Shortage in Attendance!)", roll);
+
+    return 0;
+}
